e7a2a875e3486c81396d1bca4f64bba541babbce
Dawin Rafael Perez Batista <dawin2020@hotmail.com>
2021-11-28 00:23:02 -0400

    change styles for the table

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,21 +5,20 @@ const MovieCard = ({ movies }) => {
     return (
         <React.Fragment>
             <div className="container">
-
                 <div className="row">
-
                     { movies.map( (m, i) => (
+                        <div key={i} className="col-4" style={{ width: '18rem' }}>                        
+                            <div className="card">                                
+                                    <img src={"https://image.tmdb.org/t/p/w500"+m.poster_path} className="card-img-top" alt="..."/>
 
-                        <div key={i} className="col my-5" style={{ width: '18rem' }}>
-                        <img src={m.poster_path} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title text-center">{ m.title }</h5>
-                            <h5 className="card-title text-center">{ m.poster_path }</h5>
-                            <p className="card-text text-center">{ m.overview }</p>
-                            <div className="text-center">
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                <div className="card-body">
+                                    <h5 className="card-title text-center">{ m.title }</h5>                            
+                                    <p className="card-text text-center">{ m.overview }</p>
+                                    <div className="text-center">
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
                         </div>
                     ))
                     }
